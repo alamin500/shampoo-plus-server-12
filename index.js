e4a8d5fd553e6db0f1c86e641ccd5c570354bd1f
alamin500 <nalamin500@gmail.com>
2021-11-11 09:45:57 +0600

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const MongoClient = require("mongodb").MongoClient;
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -13,80 +13,122 @@ app.use(express.json());
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
 // });
-const uri = "mongodb+srv://alamin-matlab_2021:gVZPplwupeaQWwOJ@cluster0.2vil1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const uri =
+  'mongodb+srv://alamin-matlab_2021:gVZPplwupeaQWwOJ@cluster0.2vil1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 console.log(uri);
-app.get("/", (req, res) => {
-  res.send("Welcome our Shampoo Plus Website!");
+app.get('/', (req, res) => {
+  res.send('Welcome our Shampoo Plus Website!');
   console.log(req);
 });
 client.connect((err) => {
-  const shampooCollection = client.db("shampooPlusWebsite").collection("shampoo");
+  const shampooCollection = client
+    .db('shampooPlusWebsite')
+    .collection('shampoo');
 
- const BooksCollection = client.db("shampooPlusWebsite").collection("books");
+  const OrdersCollection = client.db('shampooPlusWebsite').collection('orders');
 
-  const ReviewCollection = client.db("shampooPlusWebsite").collection("review");
+  const ReviewCollection = client.db('shampooPlusWebsite').collection('review');
+
+  const usersCollection = client.db('shampooPlusWebsite').collection('users');
   // perform actions on the collection object
 
   // Add Product
- app.post("/addShampoo", async (req, res) => {
+  app.post('/addShampoo', async (req, res) => {
     const result = await shampooCollection.insertOne(req.body);
   });
 
- // Add Reviews
- app.post("/addReview", async (req, res) => {
-   console.log(req.body)
-  const result = await ReviewCollection.insertOne(req.body);
-  console.log(result)
+  // Add Reviews
+  app.post('/addReview', async (req, res) => {
+    console.log(req.body);
+    const result = await ReviewCollection.insertOne(req.body);
+    console.log(result);
   });
 
-  // Add MyBooks
-  app.post("/myBook", async (req, res) => {
-    const result = await BooksCollection.insertOne(req.body);
+  // Get Reviews
+  app.get('/reviews', async (req, res) => {
+    const result = await ReviewCollection.find({}).toArray();
+    res.send(result);
   });
-//put/update
-app.put('/users', async (req, res) => {
-    const result = await usersCollection.updateOne({ email: req.body.email}, {$set: req.body}, {upsert: true });
-    res.send(result)
+
+  // Add Orders
+  app.post('/order', async (req, res) => {
+    const result = await OrdersCollection.insertOne(req.body);
   });
-// Get Reviews
- app.get("/reviews", async (req, res) => {
-  const result = await ReviewCollection.find({}).toArray();
-  res.send(result);
- })
+  //put/update
+  app.put('/users', async (req, res) => {
+    const result = await usersCollection.updateOne(
+      { email: req.body.email },
+      { $set: req.body },
+      { upsert: true }
+    );
+    res.send(result);
+  });
 
 
   // Get My books
-  app.get("/myBooks/:email", async (req, res) => {
-    const result = await BooksCollection.find({
+  app.get('/myOrder/:email', async (req, res) => {
+    const result = await OrdersCollection.find({
       email: req.params.email,
     }).toArray();
     res.send(result);
   });
 
   // Get All Books
-  app.get("/allBooks", async (req, res) => {
-    const result = await BooksCollection.find({}).toArray();
+  app.get('/allOrders', async (req, res) => {
+    const result = await OrdersCollection.find({}).toArray();
     res.send(result);
+  });
+  // Add user
+  app.post('/users', async (req, res) => {
+    console.log('req.body');
+    const result = await usersCollection.insertOne(req.body);
+    res.send(result);
+    console.log(result);
+  });
+
+  // Make Admin
+  app.put('/makeAdmin', async (req, res) => {
+    console.log(req.body.email);
+    const filter = { email: req.body.email };
+    const result = await usersCollection.find(filter).toArray();
+    if (result) {
+      const updates = await usersCollection.updateOne(filter, {
+        $set: { role: 'admin' },
+      });
+      console.log(updates);
+    }
   });
 
   // Delete MyBook
-  app.delete("/deleteBook/:id", async (req, res) => {
+  app.delete('/deleteOrder/:id', async (req, res) => {
     console.log(req.params.id);
-    const result = await BooksCollection.deleteOne({
+    const result = await OrdersCollection.deleteOne({
       _id: req.params.id,
     });
     res.send(result);
   });
 
+  // Delete Product
+  app.delete('/deleteProducts/:id', async (req, res) => {
+    console.log(req.params.id);
+    const result = await shampooCollection.deleteOne({
+      _id: ObjectId(req.params.id),
+    });
+    res.send(result);
+  });
+
   // Get Products
-  app.get("/products", async (req, res) => {
+  app.get('/products', async (req, res) => {
     const result = await shampooCollection.find({}).toArray();
     res.send(result);
   });
   // client.close();
 });
 app.listen(process.env.PORT || port, () => {
-  console.log(`listening at ${port}`)
+  console.log(`listening at ${port}`);
 });
